4fc6db0a934159f17ad553bbdab6dcbdd8fdddf6
AndreyGubarev777 <gubarevaa777@gmail.com>
2024-08-06 15:36:49 +0200

    Add files via upload

---
+++ module_2_2.py
@@ -0,0 +1,29 @@
+# 1
+first_1 = 1
+second_1 = 2
+third_1 = 3
+
+if first_1 == second_1 == third_1:
+    print(3)
+
+# 2
+first_2 = 1
+second_2 = 2
+third_2 = 1
+if first_2 == second_2:
+    print(0)
+elif second_2 == third_2:
+    print(1)
+elif first_2 == third_2:
+    print(2)
+
+# 3
+first_3 = 4
+second_3 = 5
+third_3 = 6
+if first_3 == second_3:
+    print(0)
+elif first_3 == third_3:
+    print(1)
+elif second_3 == third_3:
+    print(2)
