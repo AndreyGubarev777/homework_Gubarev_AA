bdcb75bad6f2b9da80f1cd46e99de451d100844e
AndreyGubarev777 <gubarevaa777@gmail.com>
2024-12-18 17:29:51 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,18 @@
+print("1st program")
+print(9 ** 0.5 * 5)
+
+print("2nd program")
+print(9.99 > 9.98 and 1000 != 1000.1)
+
+print("3rt program")
+exercise2 = 2 * 2 + 2
+print(exercise2)
+exercise3 = 2 * (2 + 2)
+print(exercise3)
+print(exercise2 == exercise3)
+
+print("4th program")
+string = '123.456'
+flSt = float(string)
+fl = int(flSt * 10)
+print(fl % 10)
